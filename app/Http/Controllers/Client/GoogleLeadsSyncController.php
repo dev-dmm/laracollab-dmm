7e919f106ad8f1692bd14428bdead3aec334d618
dev-dmm <211319935+dev-dmm@users.noreply.github.com>
2025-05-15 08:35:57 +0000

    Apply laravel-pint changes

--- app/Http/Controllers/Client/GoogleLeadsSyncController.php
+++ app/Http/Controllers/Client/GoogleLeadsSyncController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Client;
 
-use App\Console\Commands\SyncGoogleSheetClients;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Artisan;
 
@@ -11,7 +10,7 @@ class GoogleLeadsSyncController
     public function sync(Request $request)
     {
         Artisan::call('leads:sync-google');
+
         return back()->with('success', 'Google leads synced!');
     }
 }
-
